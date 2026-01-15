3f5ae653e8222f5b50318126712044e1a961bffd
cranberryandmixnuts <sujin83668252@gmail.com>
2026-01-15 15:46:33 +0900

    Update SceneLoadButton.cs

--- Assets/Core/Scripts/UI/SceneLoadButton.cs
+++ Assets/Core/Scripts/UI/SceneLoadButton.cs
@@ -6,7 +6,7 @@ public sealed class SceneLoadButton : MonoBehaviour
     [ValidateInput(nameof(IsValidScene), "SceneType이 None이면 로드할 수 없습니다."), SerializeField]
     private SceneType scene = SceneType.None;
 
-    public void Load() => SceneLoader.Instance.LoadScene(this.scene);
+    public void Load() => SceneLoader.Instance.LoadScene(scene);
 
     private bool IsValidScene(SceneType value) => value != SceneType.None;
 }
